280c9c156d897fb49666a4ae7926e580b668f810
2bjake <fosterjake@gmail.com>
2016-09-28 15:07:05 -0500

    Initial Commit

---
+++ TopRegions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TopRegions
+//
+//  Created by Foster, Jake on 9/28/16.
+//  Copyright © 2016 Amazon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TopRegions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TopRegions
+//
+//  Created by Foster, Jake on 9/28/16.
+//  Copyright © 2016 Amazon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
